79200c20d614a81ff581d668bf6745f884b9a8e4
Richard Hyesung O <orichh@gmail.com>
2022-01-28 23:04:31 -0500

    Got the graphql client to query correctly and then render the states data in the autocomplete dropdown menu

--- src/components/GraphQL.tsx
+++ src/components/GraphQL.tsx
@@ -2,18 +2,15 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
   gql,
 } from "@apollo/client";
 import { RestLink } from "apollo-link-rest";
+import { Autocomplete, TextField } from "@mui/material";
+import { useState } from "react";
 
 const restLink = new RestLink({
   uri: "https://frontend-take-home.fetchrewards.com/",
 });
-
-// const restLink = new RestLink({
-//   uri: "https://swapi.dev/api/",
-// });
 
 const client = new ApolloClient({
   link: restLink,
@@ -30,22 +27,46 @@ const query = gql`
     }
   }
 `;
-// const query = gql`
-//   query Lukeeeee {
-//     personnnnn @rest(type: "Personn", path: "people/1/") {
-//       height
-//     }
-//   }
-// `;
 
-client.query({ query }).then((response) => {
-  console.log(response.data);
-});
+export const getData = () => {
+  return client.query({ query });
+};
 
 export const Test = () => {
+  const [data, setData] = useState();
+  getData().then(({ data, loading, error }) => {
+    if (loading) return loading;
+    if (error) return error;
+    if (data) {
+      console.log(
+        "🚀 ~ file: GraphQL.tsx ~ line 41 ~ getData ~ data",
+        Array.isArray(data.pleaseeee.states)
+      );
+
+      setData(data.pleaseeee.states);
+    }
+  });
+
   return (
     <ApolloProvider client={client}>
-      <div>hello</div>
+      <Autocomplete
+        autoHighlight
+        id="auto-highlight"
+        options={data ? data : [{ label: "loading", value: "loading" }]}
+        // options={[{ label: "test", value: "test" }]}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="State" variant="standard" />
+        )}
+      />
     </ApolloProvider>
   );
+};
+
+const TestingResults = (props: any) => {
+  console.log(
+    "🚀 ~ file: GraphQL.tsx ~ line 50 ~ TestingResults ~ props",
+    props
+  );
+  return <div>testing results</div>;
 };
