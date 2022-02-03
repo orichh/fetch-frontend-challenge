ce72960896e97c2d35167d141a9b7fb6ccb05b6c
Richard Hyesung O <orichh@gmail.com>
2022-02-02 20:23:47 -0500

    Create custom useRequest hook

---
+++ src/hooks/index.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export const useRequest = (getRequest: Function) => {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [error, setError] = useState<any>();
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+
+      await getRequest()
+        .then((response: any) => setData(response.data))
+        .catch((error: any) => setError(error));
+      setLoading(false);
+    };
+    getData();
+  }, [getRequest]);
+
+  return { data, loading, error };
+};
