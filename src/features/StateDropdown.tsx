bb8c603ef5a05cdd689e31bd5ec832974b8a403b
Richard Hyesung O <orichh@gmail.com>
2022-02-02 20:23:24 -0500

    Add props interface and adjust styling

--- src/features/StateDropdown.tsx
+++ src/features/StateDropdown.tsx
@@ -1,16 +1,17 @@
 import { Autocomplete, TextField, Box } from "@mui/material";
 
-interface StateProps {
+type StateProps = {
   setResidentState: Function;
-  states: [{ label: string; value: string }];
-}
+  states: Array<{ label: string; value: string }>;
+};
 
 //prettier-ignore
 export const StateDropdown = ({ setResidentState, states }: StateProps) => {
+
   const handleChange = (event: any, value: any) => {
     event.preventDefault()
     if (value === null) {
-      setResidentState(null);
+      setResidentState("");
     } else {
       setResidentState(value.value);
     }
@@ -25,7 +26,7 @@ export const StateDropdown = ({ setResidentState, states }: StateProps) => {
         onChange={handleChange}
         isOptionEqualToValue={(option, value) => option.value === value.value}
         options={states}
-        sx={{ display: "flex", flex: "5", margin: "3%", minWidth: "157px" }}
+        sx={{ display: "flex", flex: "5", margin: "3%", minWidth: "159px" }}
         renderInput={(params) => (<TextField {...params} label="State" required />)}
         renderOption={(props, option: any) => (
           <Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 }, flex: 5 }} {...props} value={option.value} >
