c97e1fce2272c0f626dfb2e247eac76fefae48c6
Richard Hyesung O <orichh@gmail.com>
2022-02-02 20:21:32 -0500

    Change default occupation value, change minwidth

--- src/features/OccupationDropdown.tsx
+++ src/features/OccupationDropdown.tsx
@@ -2,17 +2,16 @@ import { Autocomplete, TextField, Box } from "@mui/material";
 
 interface OccupationProps {
   setOccupation: Function;
-  occupations: Array<string>;
+  occupations: Array<{ label: string; value: string }>;
 }
 
 //prettier-ignore
 export const OccupationDropdown = ({ setOccupation, occupations }: OccupationProps) => {
-  const occupationsData = occupations.map((occupation) => ({ label: occupation, value: occupation }));
 
   const handleChange = (event: any, value: any) => {
     event.preventDefault()
     if (value === null) {
-      setOccupation(null);
+      setOccupation("");
     } else {
       setOccupation(value.value);
     }
@@ -24,10 +23,10 @@ export const OccupationDropdown = ({ setOccupation, occupations }: OccupationPro
         autoHighlight
         fullWidth
         id="auto-highlight"
-        sx={{ display: "flex", flex: "5", margin: "3%", minWidth: "157px" }}
+        sx={{ display: "flex", flex: "5", margin: "3%", minWidth: "159px" }}
         onChange={handleChange}
         isOptionEqualToValue={(option, value) => option.value === value.value}
-        options={occupationsData}
+        options={occupations}
         renderInput={(params) => (<TextField {...params} label="Occupation" required />)}
         renderOption={(props, option: any) => (
           <Box component="li" sx={{ "& > img": { mr: 2, flexShrink: 0 } }} {...props} value={option.value}>
