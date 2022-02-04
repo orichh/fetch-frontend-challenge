49b6c2468fc313fb7151558244eb889398b18b48
Richard Hyesung O <orichh@gmail.com>
2022-02-03 19:08:24 -0500

    Remove comment

--- src/routes/SignUp.tsx
+++ src/routes/SignUp.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { SignUpForm } from "../features";
 
-// SignUp component -------------------------------------------------------------------------------
 export const SignUp = () => {
   return (
     <>
