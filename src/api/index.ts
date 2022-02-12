b138a45b2e5bd3dd27544ebf1adad22fb67e8f9d
Richard Hyesung O <orichh@gmail.com>
2022-02-11 19:56:21 -0500

    Change API calls to be more generic and accept input parameters specifying endpoint and data

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,30 +2,17 @@ import axios from "axios";
 
 const BASE_URL = axios.create({baseURL: "https://frontend-take-home.fetchrewards.com"}) //prettier-ignore
 
-export const getFormData = () => {
+export const getRequest = (url: string) => {
   return BASE_URL({
     method: "GET",
-    url: "form",
+    url: url,
   });
 };
 
-interface User {
-  name: string;
-  email: string;
-  state: string;
-  occupation: string;
-  password: string;
-}
-export const addUser = ({ name, email, state, occupation, password }: User) => {
+export const postRequest = (url: string, data: any) => {
   return BASE_URL({
     method: "POST",
-    url: "form",
-    data: {
-      name,
-      email,
-      state,
-      occupation,
-      password,
-    },
+    url: url,
+    data: data,
   });
 };
