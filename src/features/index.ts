9aaeb611a1e8cb069d3936ef8de3ac9b14e9f929
Richard Hyesung O <orichh@gmail.com>
2022-02-11 19:55:25 -0500

    Remove occupation and state dropdown imports and exports

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,5 +1,3 @@
-import { OccupationDropdown } from "./OccupationDropdown";
-import { StateDropdown } from "./StateDropdown";
 import { SignUpForm } from "./SignUpForm";
 
-export { OccupationDropdown, StateDropdown, SignUpForm };
+export { SignUpForm };
